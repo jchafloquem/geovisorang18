d3fad404c6e55cfd72c584ed630497f04dae8e21
Jorge Chafloque Millones <37223352+jchafloquem@users.noreply.github.com>
2024-08-28 10:04:40 -0500

    Implemetar SideBar al 50%

--- src/app/geovisor/components/sidebar/sidebar.component.ts
+++ src/app/geovisor/components/sidebar/sidebar.component.ts
@@ -1,12 +1,36 @@
+import {CommonModule, NgClass} from '@angular/common';
 import { Component } from '@angular/core';
+
+import {MatButtonModule} from '@angular/material/button';
+import {MatIconModule} from '@angular/material/icon';
+
+
 
 @Component({
   selector: 'app-sidebar',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, MatIconModule, NgClass, MatButtonModule],
   templateUrl: './sidebar.component.html',
   styleUrl: './sidebar.component.scss'
 })
 export class SidebarComponent {
+
+  public subMenu: 'leyendas' | 'capas' | 'filtros' |'config'|'login'= 'capas';
+
+  public toogleMenu = false;
+
+  clickToogleMenu(filtro?: 'leyendas' | 'capas' | 'filtros' |'config'| 'login'): void {
+		if (filtro == undefined) {
+			this.toogleMenu = !this.toogleMenu;
+		} else {
+			if (this.subMenu == filtro) {
+				this.subMenu = filtro;
+				this.toogleMenu = !this.toogleMenu;
+			} else {
+				this.subMenu = filtro;
+				this.toogleMenu = true;
+			}
+		}
+	}
 
 }
